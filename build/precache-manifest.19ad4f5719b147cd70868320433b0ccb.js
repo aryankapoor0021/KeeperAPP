06a56497d6c886e9610a58030e4869cb14d238e7
Aryan Kapoor <aryankapoorjagadhri@gmail.com>
2021-05-10 11:26:32 +0530

    ok

---
+++ build/precache-manifest.19ad4f5719b147cd70868320433b0ccb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3f784598bd8a443127cfffc5adb0fd",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/KeeperApp/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "623d39b538a7b64192e6",
+    "url": "/KeeperApp/static/js/main.fe9bd378.chunk.js"
+  },
+  {
+    "revision": "9dc866653fcfda415af8",
+    "url": "/KeeperApp/static/js/runtime-main.f1d969fb.js"
+  }
+]);
